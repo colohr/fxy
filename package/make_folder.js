42ccaec30cb861b2a34eb3337fd80abcbadca280
Laser Panchito <colohr.code@gmail.com>
2019-01-31 08:19:40 -0600

    Make folder

--- package/make_folder.js
+++ package/make_folder.js
@@ -1,18 +1,22 @@
 const mkdirp = require('mkdirp')
 
 //exports
-module.exports = make_directories
-module.exports.promise = make_directories
-module.exports.sync = make_directories_sync
+module.exports = make_folders
+module.exports.promise = make_folders
+module.exports.sync = make_folders_sync
 
 //scope actions
-function make_directories(path){
-  return new Promise((success,error)=>{
-    return mkdirp(path,err=>{
-      if(err) return error(err)
-      return success(path)
-    })
-  })
+function make_folders(folder){
+  return new Promise(make_folders_promise)
+  //scope actions
+  function make_folders_promise(success,failed){
+    mkdirp(on_response)
+    //scope actions
+    function on_response(error){
+      if(error) failed(error)
+      else success(folder)
+    }
+  }
 }
 
-function make_directories_sync(path){ return mkdirp.sync(path) }
+function make_folders_sync(folder){ return mkdirp.sync(folder) }
